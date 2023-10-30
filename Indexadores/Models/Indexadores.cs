1aabbda58b9c665ad810f72bba63c9651d3269a5
Thiago Silva <thi.ferreira.silva03@gmail.com>
2023-10-30 13:36:33 -0300

    indexers

---
+++ Indexadores/Models/Indexadores.cs
@@ -0,0 +1,21 @@
+﻿namespace Indexadores.Models
+{
+    public class Indexadores
+    {
+        private List<Indexadores> _indexadores = new();
+
+        public string Mensagem { get; set; }
+
+        public Indexadores(string mensagem = "")
+        {
+            if (!string.IsNullOrWhiteSpace(mensagem))
+                Mensagem = mensagem;
+        }
+
+        public Indexadores this[int index]
+        {
+            get { return _indexadores[index]; } 
+            set { this._indexadores.Add(value); }
+        }
+    }
+}
---
+++ Indexadores/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] list = { 1, 2, 3, 4, 5 };
+
+var indexadores = new Indexadores.Models.Indexadores();
+
+for (int index = 0; index < list.Length; index++)
+    indexadores[index] = new Indexadores.Models.Indexadores($"Indexador {index}");
+
+for (int index = 0; index < list.Length; index++)
+    Console.WriteLine(indexadores[index].Mensagem);
